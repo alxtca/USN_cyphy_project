6aa910cb606d2feae4191aa2181033c170b886d4
Øystein Årsnes <oy.aars@gmail.com>
2021-11-29 15:39:10 +0100

    Update measure-height.cpp

--- RP1 code/measure-height.cpp
+++ RP1 code/measure-height.cpp
@@ -5,57 +5,75 @@
 #include <iostream>
 #include <string>
 
-#define BUF_SIZE 128
+/* This function sets up a serial connection with Arduino, sending it a message to take a measurement. 
+* It waits for a Arduino to finish, and returns the data received back (from Arduino) to main as an int */
 
+#define BUF_SIZE 128
 
 int measureHeight(){
   //your serial code here
   
-  //int i=0;
-	int COM_port = {24}; /* COM port number for device /dev/ttyACM0 (Arduino MEGA)*/
-	int baud_rate = {115200}; /* baud rate */
-	char mode[] = {'8', 'N', '1', 0 }; // Setup: 8 data bits, no parity, 1 stop bit, no flow control
+	int COM_port = {24}; // COM port number for device: /dev/ttyACM0 (Arduino MEGA)
+	int baud_rate = {115200}; //chosen baud rate
+	char mode[] = {'8', 'N', '1', 0 }; //Mode setup: {8 data bits, no parity, 1 stop bit, no flow control}
 	
-	char string_to_send[BUF_SIZE]; // send data buffer
-	unsigned char string_receive[BUF_SIZE]; // receive data buffer
+	char outgoing[BUF_SIZE]; // Buffer for outgoing message
+	unsigned char incoming[BUF_SIZE]; // Buffer for incoming message
   
-	//open Serial COM port
-	if(RS232_OpenComport(COM_port, baud_rate, mode, 0))
+	//Open Serial COM port:
+	if(RS232_OpenComport(COM_port, baud_rate, mode, 0)) // 0 = no flow control
+	//Parameters: int RS232_OpenComport(int comport_number, int baudrate, const char * mode, int flowctrl)
 	{
 		printf("Can not open comport\n");
-		return(0);
+		return;
+		/* 	This is a fail-check, but not sure how it works. RS232_OpenComport() does return an 
+		*	int depending on success or not, but I can't see it used here. It does work however, 
+		*	and prints fail message when Arduino is not connected
+	  */
 	}
+	//untested alternative
+	/*int open = RS232_OpenComport(COM_port, baud_rate, mode, 0); //returns 1 in case of error
+	if(open == 1){
+		printf("Can not open comport\n");
+		return; 
+	}
+	*/
+
   
-	usleep(2000000);  /* wait 2 seconds for stable condition */
+	usleep(2000000);  // wait 2 seconds for stable condition
   
-	strcpy(string_to_send, "start");
-	RS232_cputs(COM_port, string_to_send); // sends string on serial
-	printf("Sent to Arduino: '%s'\n", string_to_send);
-	usleep(3000000);  /* wait 2 seconds before checking reply */
+	strcpy(outgoing, "start"); //copy "outgoing" buffer to string
+	RS232_cputs(COM_port, outgoing); // sending "outgoing" string to Arduino
+	printf("Message sent to Arduino: '%s'\n", outgoing); //Feedback: Showing data sent to Arduino
+	usleep(3000000);  // wait 3 seconds before checking for reply
 	
-	int no_ofBytes_back = RS232_PollComport(COM_port, string_receive, (int)BUF_SIZE);
-	if(no_ofBytes_back > 0){
-		string_receive[no_ofBytes_back] = 0;   /* always put a "null" at the end of a string! */
-		printf("Received %i bytes: '%s'\n", no_ofBytes_back, (char *)string_receive);
+	// Check reply: content to be stored in incoming[]
+	int received = RS232_PollComport(COM_port, incoming, (int)BUF_SIZE); //int received shows number of bytes received from Arduino
+	//Parameters: int RS232_PollComport(int comport_number, unsigned char *buf, int size)
+
+	if(received > 0){ // If 'received' is greater than zero, then there was data from Arduino
+		incoming[received] = 0;   // add zero behind last received byte to signals end of data
+		printf("Received %i bytes: '%s'\n", received, (char *)incoming); //Feedback: Showing number of bytes received
 	}
 	
 	RS232_CloseComport(COM_port);
 	
-	char buf[BUF_SIZE];
+	/* Making a signed copy of incoming[] buf, because std::string 
+	does not work with unsigned char*/
+	char signed_copy[BUF_SIZE];
 	for(int i = 0 ; i < BUF_SIZE; i++){
-		buf[i] = string_receive[i];
+		signed_copy[i] = incoming[i];
+		/* untested change to include
+		if(incoming[i] == '\0'){
+			break;
+		}*/
 	}
 	
-	std::cout << "riktig variabel: " << string_receive << std::endl;
-	std::cout << "riktig variabel: " << buf << std::endl;
-	
-	//int int_distance = (int)(string_receive[0]);
-	
-    //string to int
-	std::string d{buf};
-	int int_distance{std::stoi(d)};
-	std::cout << "riktig variabel: " << int_distance << std::endl;
+	std::string charArray_to_string{signed_copy}; //convert char-array to string
+	int distance{std::stoi(charArray_to_string)}; //convert string to int
+	//Test: Feedback to user, is distance correct?
+	std::cout << "Distance to object: " << distance << " cm" << std::endl;
   
-  return int_distance;
+  return distance;
 }
 
