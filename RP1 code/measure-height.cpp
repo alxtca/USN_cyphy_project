7f20cc01d6c4fd6260733b4ba3bfa6d9b446bda8
Øystein Årsnes <oy.aars@gmail.com>
2021-12-07 17:39:59 +0100

    Add files via upload

--- RP1 code/measure-height.cpp
+++ RP1 code/measure-height.cpp
@@ -24,53 +24,42 @@ int measureHeight(){
 	if(RS232_OpenComport(COM_port, baud_rate, mode, 0)) // 0 = no flow control
 	//Parameters: int RS232_OpenComport(int comport_number, int baudrate, const char * mode, int flowctrl)
 	{
-		printf("Can not open comport\n");
-		return;
-		/* 	This is a fail-check, but not sure how it works. RS232_OpenComport() does return an 
-		*	int depending on success or not, but I can't see it used here. It does work however, 
-		*	and prints fail message when Arduino is not connected
-	  */
+		printf("Can not open comport\n");	//print if no connection
 	}
-	//untested alternative
-	/*int open = RS232_OpenComport(COM_port, baud_rate, mode, 0); //returns 1 in case of error
-	if(open == 1){
-		printf("Can not open comport\n");
-		return; 
-	}
-	*/
+	
 
   
-	usleep(2000000);  // wait 2 seconds for stable condition
+	usleep(1'000'000);  // wait 1 second for stable condition
   
 	strcpy(outgoing, "start"); //copy "outgoing" buffer to string
 	RS232_cputs(COM_port, outgoing); // sending "outgoing" string to Arduino
 	printf("Message sent to Arduino: '%s'\n", outgoing); //Feedback: Showing data sent to Arduino
-	usleep(3000000);  // wait 3 seconds before checking for reply
+	usleep(10'000'000);  // wait 10 seconds before checking for reply
 	
-	// Check reply: content to be stored in incoming[]
-	int received = RS232_PollComport(COM_port, incoming, (int)BUF_SIZE); //int received shows number of bytes received from Arduino
+	// Check reply:
+	// content will be stored in incoming[]
+	int bytes_received = RS232_PollComport(COM_port, incoming, (int)BUF_SIZE); //int bytes_received is number of bytes received from Arduino
 	//Parameters: int RS232_PollComport(int comport_number, unsigned char *buf, int size)
 
-	if(received > 0){ // If 'received' is greater than zero, then there was data from Arduino
-		incoming[received] = 0;   // add zero behind last received byte to signals end of data
+	if(bytes_received > 0){ // If number of 'bytes_received' is greater than zero, then there is data from Arduino
+		incoming[bytes_received] = 0;   // add zero behind last received byte to signals end of data
 		printf("Received %i bytes: '%s'\n", received, (char *)incoming); //Feedback: Showing number of bytes received
 	}
 	
 	RS232_CloseComport(COM_port);
 	
-	/* Making a signed copy of incoming[] buf, because std::string 
-	does not work with unsigned char*/
-	char signed_copy[BUF_SIZE];
+	/* Convert unsigned char message to normal char, because 
+	 * std::string does not take unsigned char*/
+	char char_copy[BUF_SIZE];
 	for(int i = 0 ; i < BUF_SIZE; i++){
-		signed_copy[i] = incoming[i];
-		/* untested change to include
+		char_copy[i] = incoming[i];		
 		if(incoming[i] == '\0'){
 			break;
-		}*/
+		}
 	}
 	
-	std::string charArray_to_string{signed_copy}; //convert char-array to string
-	int distance{std::stoi(charArray_to_string)}; //convert string to int
+	std::string string_copy{char_copy}; //convert message to string
+	int distance{std::stoi(string_copy)}; //convert message to int
 	//Test: Feedback to user, is distance correct?
 	std::cout << "Distance to object: " << distance << " cm" << std::endl;
   
