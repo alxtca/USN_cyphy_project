bdd6e432e0a0d2c94c069e2fba9ce39491b4d6ff
Sudobrain <c.johannes.h@gmail.com>
2021-12-14 11:04:23 +0100

    Update Coms_NO.cpp

--- RP_2_code/UserInterface/Coms_NO.cpp
+++ RP_2_code/UserInterface/Coms_NO.cpp
@@ -21,7 +21,7 @@ void Coms::menu_new()
 		<< "0 : Tilbake" << std::endl;
 }
 
-void Coms::menu_excisting()
+void Coms::menu_existing()
 {
 	std::cout << "Eksterende måling:" << std::endl
 		<< "1 : Søk etter måling" << std::endl
